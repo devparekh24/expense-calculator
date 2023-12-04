e93ae16a3952f9b39d066b821c01f706fa3cbd23
devparekh24 <devparekh2424@gmail.com>
2023-12-04 18:13:53 +0530

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
     return (
         <div className='footer'>
-            
-            <p>Copyright &copy; {currentYear} : Expense Calculator by <a href="https://github.com/devparekh24">devparekh</a> All rights reserved </p>
+
+            <p>Copyright &copy; {currentYear} : Expense Calculator by <a href="https://github.com/devparekh24/expense-calculator">devparekh</a> All rights reserved </p>
         </div >
     )
 }
